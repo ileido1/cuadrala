dbb53142fde2175f0573bfe195247e32fcc73530
chernandez <soycarlosluis862@gmail.com>
2026-05-11 16:41:20 -0400

    chore: remove dist/ from git tracking
    - dist/ is build output and should not be committed
    - Already added to .gitignore in previous commit

--- services/api/dist/infrastructure/repositories/category.repository.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.findCategoryByIdRepo = findCategoryByIdRepo;
-const prisma_client_js_1 = require("../prisma_client.js");
-async function findCategoryByIdRepo(_id) {
-    return prisma_client_js_1.PRISMA.category.findUnique({ where: { id: _id } });
-}
--- services/api/dist/presentation/middleware/async_handler.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.asyncHandler = asyncHandler;
-/** Permite usar controladores async con propagación al middleware de errores. */
-function asyncHandler(_fn) {
-    return (_req, _res, _next) => {
-        void _fn(_req, _res, _next).catch(_next);
-    };
-}
